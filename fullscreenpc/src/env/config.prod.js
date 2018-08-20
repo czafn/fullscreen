eb84872c06b808510a8d35104a97cd04f84d1382
wangxiaoqing <xiaoqingwang@126.com>
2018-08-21 00:20:11 +0800

    ===修改版本号===

--- fullscreenpc/src/env/config.prod.js
+++ fullscreenpc/src/env/config.prod.js
@@ -9,7 +9,7 @@ let config = {
     serverurl:`${serverurl}`,
     wspath:`${wspath}`,
     requesttimeout:5000,
-    appversion:'1.7.2(build0708)',
+    appversion:'1.7.3(build0821)',
     sendlocationinterval:20000,
     softmode:'fullpc',
     wendu_max: 80,
