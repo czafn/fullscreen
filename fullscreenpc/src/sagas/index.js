e3794753305eaf0ec8622d357ad812fe9dc7c303
wangxiaoqing <xiaoqingwang@126.com>
2018-08-21 00:50:41 +0800

    saga

--- fullscreenpc/src/sagas/index.js
+++ fullscreenpc/src/sagas/index.js
@@ -3,31 +3,31 @@ import {apiflow} from './api';
 import {createsagacallbackflow} from './pagination';
 
 import {wsrecvsagaflow} from './wsrecvsaga';
-import {jpushflow} from './jpushflow';
+// import {jpushflow} from './jpushflow';
 
-import {deviceinfoquerychartflow} from './deviceinfoquerychart';
+// import {deviceinfoquerychartflow} from './deviceinfoquerychart';
 import {createloadingflow} from './loading';
 import {createmapmainflow} from './mapmain';
-import {createmaptrackhistoryplaybackflow} from './mapplayback';
+// import {createmaptrackhistoryplaybackflow} from './mapplayback';
 import {socketflow} from './socketflow';
-import {uiflow} from './ui';
-import {downloadexcel} from './downloadexcel';
+// import {uiflow} from './ui';
+// import {downloadexcel} from './downloadexcel';
 import {catldata} from './catlfull';
 import {catldatarestful} from './catlfull_restful';
 import config from '../config.js';
 
 export default function* rootSaga() {
   try{
-    if(config.softmode === 'app'){
-      yield fork(jpushflow);
-    }
-    yield fork(deviceinfoquerychartflow);
-    yield fork(downloadexcel);
+    // if(config.softmode === 'app'){
+    //   yield fork(jpushflow);
+    // }
+    // yield fork(deviceinfoquerychartflow);
+    // yield fork(downloadexcel);
     yield fork(socketflow);
     yield fork(createmapmainflow);
-    yield fork(createmaptrackhistoryplaybackflow);
+    // yield fork(createmaptrackhistoryplaybackflow);
     yield fork(createloadingflow);
-    yield fork(uiflow);
+    // yield fork(uiflow);
     yield fork(wsrecvsagaflow);
     yield fork(createsagacallbackflow);
     yield fork(apiflow);
