e0899e9df317fda4f42bfe587c9ff7390158da54
wangxiaoqing <xiaoqingwang@126.com>
2018-09-28 05:19:04 +0800

    =====

--- fullscreenpc/src/env/config.dev.js
+++ fullscreenpc/src/env/config.dev.js
@@ -1,5 +1,5 @@
 let islocalhost = false;
-const serverurl = islocalhost?'http://localhost:5011':'http://bmstest.i2u.top:81';
+const serverurl = islocalhost?'http://localhost:5011':'http://bmscatl.i2u.top:81';
 const serverurlrestful = islocalhost?`${serverurl}/api`:`${serverurl}/apisrv/api`;
 const wspath = islocalhost?'/socket.io':'/apisrv/socket.io';
 // const organizationid = '599af5dc5f943819f10509e6';
@@ -9,7 +9,7 @@ let config = {
     serverurl:`${serverurl}`,
     wspath:`${wspath}`,
     requesttimeout:5000,
-    appversion:'1.7.8(build0831)',
+    appversion:'1.8.0(build0928)',
     sendlocationinterval:20000,
     softmode:'fullpc',
     wendu_max: 80,
